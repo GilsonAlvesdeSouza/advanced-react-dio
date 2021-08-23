a11823ec179f0db6639a96fac59f86443ef123b6
Gilson Alves <gilsonanlvedesouza@hotmail.com>
2021-08-23 19:03:00 -0300

    fragments

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+function App() {
+    return (
+        <>
+            React.js
+            <Fragment>
+                <p>trabalhando com react</p>
+                <span>styled-component</span>
+            </Fragment>
+        </>
+    );
+}
+
+export default App;
